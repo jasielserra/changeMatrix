4beae63c291a8dc114de4b1d83a7efe0b3027b12
jasiel Serra <jasiel_serra@hotmail.com>
2022-03-12 22:00:02 -0300

    Refactor ver_pixel

--- matriz.py
+++ matriz.py
@@ -72,8 +72,8 @@ def ver_pixel(cmd, board):
     """Change the color of a column - 'V' Command."""
     col, row_start, row_end, color = int(cmd[0]), int(cmd[1]), int(cmd[2]), cmd[3]
 
-    for row in range(row_start, row_end + 1):
-        set_item(board, (col, row), color)
+    for coord in region(col, row_start, col, row_end):
+        set_item(board, coord, color)
     return board
 
 
