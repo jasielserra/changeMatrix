45ae59f494e490df95541e22975ec463d3a6f1f7
jasiel Serra <jasiel_serra@hotmail.com>
2022-03-28 17:32:39 -0300

    Create wrap for avoid list index out of range.

--- gameoflife.py
+++ gameoflife.py
@@ -1,6 +1,8 @@
 from copy import deepcopy
 from os import system
-from matriz import create_array, set_many, string, offset, coords_of, get_item
+from time import sleep
+
+from matriz import create_array, set_many, string, offset, coords_of, get_item, set_item
 
 DEAD = chr(0x00B7)
 LIVE = chr(0x2588)
@@ -15,6 +17,9 @@ SURROUDING = tuple((a,b)
 
 def neighbors(c):
     yield from (offset(c, r) for r in SURROUDING)
+
+def wrap(c,w, h):
+    yield from ((a % w, b % h) for a,b in c)
 
 def main():
 
@@ -35,6 +40,19 @@ def main():
             ns = get_item(board, n)
             if ns == LIVE:
                 total += 1
+
+        s = status = get_item(board, coord)
+        if status == LIVE:
+            if total < 2 or total > 3:
+                s = DEAD
+            else:
+                if total == 3:
+                    s = LIVE
+
+            set_item(new_board, coord, s)
+
+        board = new_board
+        sleep(0.05)
 
 
 
